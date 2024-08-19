620aec3f845b4b773e43b1a42cf54af7bc67084d
dwityantherrys <dwityantherrysbhektiutomo@gmail.com>
2024-08-19 10:32:35 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+    server: {
+        host: true,
+        hmr: {
+            host: '192.168.22.90',
+        },
+    },
+    build: {
+        outDir: 'dist',  // Make sure the build output goes to the 'dist' directory
+    }, //tst
+});
