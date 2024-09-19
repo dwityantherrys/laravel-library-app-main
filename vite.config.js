92eef15fb049e1f01b9215b118e803c258814fcd
dwityantherrys <dwityantherrysbhektiutomo@gmail.com>
2024-09-19 08:29:44 +0000

    Menyimpan perubahan lokal sebelum pull

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,8 @@ export default defineConfig({
     server: {
         host: true,
         hmr: {
-            host: '192.168.22.90',
+	     host : 'localhost'
+            // host: '103.23.198.209',
             // host: '192.168.1.29',
             // host: '192.168.158.108',
         },
